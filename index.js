8fe128d26158f5d2b6a4e25cbcd44e9b395afbc6
linxner <342709615@qq.com>
2020-04-02 10:23:44 +0800

    a

--- index.js
+++ index.js
@@ -10,11 +10,42 @@ app.use(async (ctx, next) => {
     await next()
 })
 
-let a = { a: 'aa' }
+
+
+let promise = new Promise((resolve, reject) => {
+    let a = 'bb';
+    let b;
+    if (a) {
+        resolve(a);
+    } else {
+        reject('cc');
+    }
+})
+// promise.then(res => { 
+//     console.log(res);
+// }).catch(res => {
+//     console.log(res);
+// })
+async function aa() {
+    setTimeout(() => {
+        console.log('aa');
+    }, 100)
+    let b = await promise
+    console.log(b);
+    return b
+}
+aa().then(res => {
+    console.log(res);
+})
+let a = { a: 'aa' };
 router.get('/login/', async (ctx, next) => {
-    ctx.status = 200
-    
+    ctx.status = 200;
+    // console.log(a)
+    console.log('aa');
     await next()
+    ctx.body = 'cc';
+    console.log('bb');
+
 })
 app.use(router.routes());
 app.listen(8080)
